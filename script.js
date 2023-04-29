02b04a4c9df3132179c18aa4d8029311184c0227
Wiktor <112684851+Szyszy04@users.noreply.github.com>
2023-04-29 22:45:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+
+$(window).on('load', function() {
+    $('#preloader').css({
+      "transform": "translateY(-100%)",
+      "transition-delay": "2.2s"
+    });
+    $('.loader').css({
+      "opacity": "0",
+      "transform": "translate(-50%,-100%)",
+      "transition-delay": "4.3s"
+    });
+  
+    $('.loader_text_unit').each(function() {
+      var $this = $(this),
+          countTo = $this.attr('data-count');
+  
+      $({ countNum: $this.text()}).animate({
+        countNum: countTo
+      },
+     );
+    });
+  });
+  
+  function toggleClassAndRemoveClasses(classToAdd, classesToRemove) {
+    $('.mercury, .venus, .earth, .mars, .jupiter, .saturn, .uranus, .neptune')
+      .toggleClass(classToAdd)
+      .removeClass(classesToRemove);
+  
+    $('.mercury-info, .venus-info, .earth-info, .mars-info, .jupiter-info, .saturn-info, .uranus-info, .neptune-info')
+      .toggleClass(classToAdd)
+      .removeClass(classesToRemove);
+  
+    $('.mercury-button, .venus-button, .earth-button, .mars-button, .jupiter-button, .saturn-button, .uranus-button, .neptune-button')
+      .toggleClass(classToAdd)
+      .removeClass(classesToRemove);
+  }
+  
+  function startPlanet(planet) {
+    var classToAdd = 'show-' + planet;
+    var classesToRemove = 'show-Mercury show-Venus show-Earth show-Mars show-Jupiter show-Saturn show-Uranus show-Neptune'.replace('show-' + planet, '');
+  
+    toggleClassAndRemoveClasses(classToAdd, classesToRemove);
+    console.log(planet);
+    console.log(classesToRemove);
+    console.log(classToAdd);
+  }
+  
+  function startMercury() {
+    startPlanet('Mercury');
+  }
+  
+  function startVenus() {
+    startPlanet('Venus');
+  }
+  
+  function startEarth() {
+    startPlanet('Earth');
+  }
+  
+  function startMars() {
+    startPlanet('Mars');
+  }
+  
+  function startJupiter() {
+    startPlanet('Jupiter');
+  }
+  
+  function startSaturn() {
+    startPlanet('Saturn');
+  }
+  
+  function startUranus() {
+    startPlanet('Uranus');
+  }
+  
+  function startNeptune() {
+    startPlanet('Neptune');
+  }
